2677ebf11d041c4945dba7996a17444eed2c742e
iceqiw <qqwei1123@163.com>
2019-10-21 11:50:13 +0800

    update conf

--- build-env/script/start-components.sh
+++ build-env/script/start-components.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-usage="Usage: hadoop-daemon.sh [--config <conf-dir>] [--hosts hostlistfile] [--script script] (start|stop) <hadoop-command> <args...>"
-
 command=$1
 shift
-master_fun(){
+master_start_fun(){
   hdfs --daemon start namenode
   yarn --daemon start resourcemanager
 }
 
-slave_fun(){
+slave_start_fun(){
   hdfs --daemon start datanode
   yarn --daemon start nodemanager
+}
+
+master_stop_fun(){
+  hdfs --daemon stop namenode
+  yarn --daemon stop resourcemanager
+}
+
+slave_stop_fun(){
+  hdfs --daemon stop datanode
+  yarn --daemon stop nodemanager
 }
 
 
 case $command in
 
   (master)
-    master_fun
+    master_start_fun
     ;;
   (slave)
-    slave_fun
+    slave_start_fun
+    ;;
+  (master-stop)
+    master_stop_fun
+    ;;
+  (slave-stop)
+    slave_stop_fun
     ;;
   (*)
     echo $usage
