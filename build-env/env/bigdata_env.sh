913cc4efd7122125342b284fbe3b1dabb79e9a19
iceqiw <qqwei1123@163.com>
2019-11-05 21:48:44 +0800

    add hbase

--- build-env/env/bigdata_env.sh
+++ build-env/env/bigdata_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export BIGDATAHOME=/opt/bigdata
+export BIGDATA_CONF=/etc
+export BIGDATA_LOG=/var/logs
 export HADOOP_HOME=$BIGDATAHOME/hadoop
 export ZOO_HOME=$BIGDATAHOME/zookeeper
 export HBASE_HOME=$BIGDATAHOME/hbase
@@ -11,20 +13,20 @@ if [[ -n $HADOOP_HOME ]]; then
   export PATH=$HADOOP_HOME/bin:$PATH
   export PATH=$HADOOP_HOME/sbin:$PATH
 
-  export HADOOP_CONF_DIR=/etc/hadoop
-  export HADOOP_LOG_DIR=/var/logs/hadoop
-  export YARN_CONF_DIR=/etc/hadoop
-  export YARN_LOG_DIR=/var/logs/yarn
+  export HADOOP_CONF_DIR=$BIGDATA_CONF/hadoop
+  export HADOOP_LOG_DIR=$BIGDATA_LOG/hadoop
+  export YARN_CONF_DIR=$BIGDATA_CONF/hadoop
+  export YARN_LOG_DIR=$BIGDATA_LOG/yarn
 fi
 
 if [[ -n $HIVE_HOME ]]; then
   export PATH=$HIVE_HOME/bin:$PATH
-  export HIVE_CONF_DIR=/etc/hive
+  export HIVE_CONF_DIR=$BIGDATA_CONF/hive
 fi
 
 if [[ -n $ZOO_HOME ]]; then
   export PATH=$ZOO_HOME/bin:$PATH
-  export ZOOCFGDIR=/etc/zookeeper
+  export ZOOCFGDIR=$BIGDATA_CONF/zookeeper
 fi
 
 if [[ -n $KAFKA_HOME ]]; then
@@ -34,4 +36,6 @@ fi
 if [[ -n $HBASE_HOME ]]; then
   export PATH=$HBASE_HOME/bin:$PATH
   export PATH=$HBASE_HOME/sbin:$PATH
+  export HBASE_CONF_DIR=$BIGDATA_CONF/hbase
+  export HBASE_LOG_DIR=$BIGDATA_LOG/hbase
 fi
