bc73079abcfd26fab8632bb65a0640f51ae24e7b
iceqiw <qqwei1123@163.com>
2020-01-19 21:24:27 +0800

    env for java home

--- build-env/etc-debug/hadoop/hadoop-env.sh
+++ build-env/etc-debug/hadoop/hadoop-env.sh
@@ -22,7 +22,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=/usr/local/java/jdk
+export JAVA_HOME=${JAVA_HOME}
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
