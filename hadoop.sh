6821cdd68df3bf3ad92289e33bb5181a7101afbe
iceqi <qqwei1123@163.com>
2022-03-17 18:57:57 +0800

    add stop func

--- hadoop.sh
+++ hadoop.sh
@@ -7,12 +7,24 @@ function start(){
   yarn --daemon start proxyserver
 }
 
+function stop(){
+  hdfs --daemon stop namenode
+  hdfs --daemon stop datanode
+
+  yarn --daemon stop resourcemanager
+  yarn --daemon stop nodemanager
+  yarn --daemon stop proxyserver
+}
 case $1 in
   all)
     echo "start hadoop"
     start
-    echo "start hadoop finish"
+    echo "start hadoop finished"
     ;;
+  stop)
+    echo "stop job"
+    stop
+    echo "stop job finished"
   *)
     echo "error input"
     ;;
