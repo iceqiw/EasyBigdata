5c18b9755fd1e1e19d4540bc45e2f17743b36064
iceqi <qqwei1123@163.com>
2022-10-21 10:57:40 +0800

    modify shell

---
+++ runHadoopCluster.sh
@@ -0,0 +1,35 @@
+function start(){
+  hdfs --daemon start namenode
+  hdfs --daemon start datanode
+
+  yarn --daemon start resourcemanager
+  yarn --daemon start nodemanager
+  yarn --daemon start proxyserver
+}
+
+function stop(){
+  hdfs --daemon stop namenode
+  hdfs --daemon stop datanode
+
+  yarn --daemon stop resourcemanager
+  yarn --daemon stop nodemanager
+  yarn --daemon stop proxyserver
+}
+read -p 'input start/stop to start cluster :  ' cmd
+
+case $cmd in
+  start)
+    echo "start hadoop"
+    start
+    echo "start hadoop finished"
+    ;;
+  stop)
+    echo "stop job"
+    stop
+    echo "stop job finished"
+    ;;
+  *)
+    echo "error input"
+    ;;
+esac
+
