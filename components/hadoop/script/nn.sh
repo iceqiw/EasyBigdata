c9dd3ef90d261668cda82770c9bdc4c4cb0d62cb
qiw <qqwei1123@163.com>
2024-04-11 11:23:23 +0800

    update config

--- components/hadoop/script/nn.sh
+++ components/hadoop/script/nn.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 function start(){
-  echo "format"
   hdfs namenode -format
-  echo "start nn"
   hdfs --daemon start namenode
-  echo "start rm"
-  yarn --daemon start resourcemanager
-  echo "start proxy"
-  yarn --daemon start proxyserver
 }
 
 
 start
-sleep infinity
